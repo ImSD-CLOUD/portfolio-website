4d78b4082f31e73d664be8107c243eaa788fc522
ImSD-CLOUD <sarupsarup66@gmail.com>
2025-09-14 19:42:18 +0530

    updated resume

--- src/components/Background3D.jsx
+++ src/components/Background3D.jsx
@@ -1,13 +1,7 @@
-export default function Background3D() {
-  return (
-    <div className="fixed inset-0 -z-10">
-      <spline-viewer
-        url="https://prod.spline.design/2rNU5gtSq3nF-kMF/scene.splinecode" 
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-      ></spline-viewer>
-    </div>
-  );
-}
+export default function Background3D()
+ { return ( <div className="fixed inset-0 -z-10"> 
+ <spline-viewer url="https://prod.spline.design/2rNU5gtSq3nF-kMF/scene.splinecode" 
+ style={{ width: "100%", height: "100%", }} >
+  </spline-viewer>
+   </div> ); 
+   }
